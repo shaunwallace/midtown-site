1b84f024681bb09bdcf5aa97a0917faee82828cc
shaun wallace <swallace6@gatech.edu>
2011-08-29 00:11:31 -0400

    forgot to fix the commented out lines from last commit

--- midtown/wordpress/wp-content/themes/gmc theme/index.php
+++ midtown/wordpress/wp-content/themes/gmc theme/index.php
@@ -1,10 +1,10 @@
-<?php get_header();
+<?php get_header(); 
 
 //set the category id to the category that has announcements
 //$category_id = 1; 
 //show the slider of the last 5 posts 3 at a time
-//get_5_posts_slider(); 
-?>
+get_5_posts_slider(); 
+
                              
-<?php get_footer(); ?>
+get_footer(); ?>
 				
